b73a57696c0bfc0fd7d6fb535bafb7c7e1016c16
Spencer Lindemuth <lindemut@gmail.com>
2019-04-02 20:28:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,14 +7,14 @@ def reformat_languages(languages)
   array = []
   languages.each do |style, language|
     language.each do |lang, type|
-      if array.length == 0
-        array << style
-        new_hash[lang] = {:type => type[:type], :style => array }
-      elsif new_hash[lang][:style].length != 0
+      if array.length != 0
         new_hash[lang] = {:type => type[:type]}
         array = new_hash[lang][:style]
         array << style
         new_hash[lang][:style] = array
+      else
+          array << style
+          new_hash[lang] = {:type => type[:type], :style => array }
       end
     end
   end
