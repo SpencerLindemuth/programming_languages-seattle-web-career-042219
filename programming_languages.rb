bb9e16aa8c750b78354920ab16acc4f44787d75d
Spencer Lindemuth <lindemut@gmail.com>
2019-04-02 20:38:02 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,6 +7,7 @@ def reformat_languages(languages)
   array = []
   languages.each do |style, language|
     language.each do |lang, type|
+      binding.pry
       if new_hash.key?(lang)
         new_hash[lang] = {:type => type[:type]}
         array = new_hash[lang][:style]
